75d53b7949561acf1e6b885474e9c4e279129072
Andrey Novikov <82.anovikov@gmail.com>
2025-10-09 11:57:19 +0300

    Lesson 7. Views

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,8 +2,11 @@
 
 # Create your views here.
 from django.http import HttpResponse
+from django.views import View
 
+# def index(request):
+#     return HttpResponse("article")
 
-def index(request):
-    return HttpResponse("article")
-    
+class IndexView(View):
+    def get(self, request):
+        return HttpResponse("article")
