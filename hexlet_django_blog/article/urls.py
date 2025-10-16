2bbe84a9d627a71cb038f12c304f509f0aaa18fd
Andrey Novikov <82.anovikov@gmail.com>
2025-10-16 16:13:23 +0300

    Lesson 8. Routing

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
-# from hexlet_django_blog.article import views
 from hexlet_django_blog.article.views import IndexView
+from hexlet_django_blog.article import views
+
 
 urlpatterns = [
-    # path("", views.index),
-    path('', IndexView.as_view(), name='index'),
+    # path("", views.index, name='index'),
+    path("", views.home, name='home'),
+    # path('', IndexView.as_view()),
+    path('<str:tags>/<int:article_id>/', IndexView.as_view(), name='article'),
+    # path('<str:tags>/<int:article_id>/', views.index, name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,12 +1,40 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.views import View
+from django.shortcuts import redirect
+from django.urls import reverse
 
 # def index(request):
 #     return HttpResponse("article")
 
+# class IndexView(View):
+#     def get(self, request):
+#         context = {"app": "article"}
+#         return render(request, "articles/index.html", context)
+
 class IndexView(View):
-    def get(self, request):
-        return HttpResponse("article")
+    def get(self, request, tags, article_id):
+        context = {"tags": tags, "article_id": article_id}
+        return render(request, "articles/index.html", context)
+
+
+# def index(request, tags, article_id):
+#     return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
+
+
+def home(request):
+    url = reverse('article', kwargs={'tags': 'python', 'article_id': 42})
+    return redirect(url)
+
+# class IndexView(View):
+#     def get(self, request):
+# #         if tags is None or article_id is None:
+#             url = reverse_lazy('article', args=[tags, article_id])
+#             return redirect(url)
+#             # return HttpResponse("article")
+#         return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
+    # def get(self, request):
+        # return HttpResponse("article")
+    
