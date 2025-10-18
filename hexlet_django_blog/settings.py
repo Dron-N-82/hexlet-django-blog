aff75ae57479e9a8692f64db180456557ab11374
Andrey Novikov <82.anovikov@gmail.com>
2025-10-18 19:09:36 +0300

    Lesson 11. Environments

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)63+p7*!kug-&cr+g@f@i*)as4)_!th8mg+jt(9g7d$+t49qch'
-
+# SECRET_KEY = 'django-insecure-)63+p7*!kug-&cr+g@f@i*)as4)_!th8mg+jt(9g7d$+t49qch'
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.10.5']
-
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.10.5']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -74,13 +80,22 @@ WSGI_APPLICATION = 'hexlet_django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
